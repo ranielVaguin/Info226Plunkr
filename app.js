5d235a687ce42a6e5e510f8133dd306cf2e21eed
mrmnemonic <markjpinpin@gmail.com>
2017-05-12 11:29:13 +1200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 var app = angular.module('plunker', []);
 
 app.controller('MainCtrl', function($scope) {
-  $scope.name = 'World';
+  $scope.feedback = '';
+  $scope.username = 'Username';
+  $scope.password = 'Password';
+  
+    $scope.clickSIGNIN = function () { 
+      $scope.feedback = 'You have successfully logged in!'; }
+     
+    $scope.clickCANCEL = function () {
+      /*Reset text in input fields and feedback*/
+      $scope.username = 'Username';
+      $scope.password = 'Password';
+      $scope.user = '';
+      $scope.pword = '';
+      $scope.feedback = ''; }
 });
